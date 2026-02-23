040188840182186546899abe1d2ee19495870598
Samuel Thelen <sthelen@lcs.on.ca>
2026-02-23 15:23:17 -0500

    Everything for today.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -12,6 +12,27 @@ struct WorldClockView: View {
         NavigationStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .navigationTitle("World Clock")
+                .toolbar {
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        
+                        Button("Edit") {
+                            // Does nothing right now
+                        }
+                        
+                    }
+                        
+                        ToolbarItem(placement: .primaryAction) {
+                            
+                            Button  {
+                                // Does nothing right now
+                            } label: {
+                                Image(systemName: "plus")
+                            
+                        }
+                        
+                    }
+                }
         }
     }
 }
