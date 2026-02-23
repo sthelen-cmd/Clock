e0fba06a9a182bd0e868611272627b5d7fb3a801
Samuel Thelen <sthelen@lcs.on.ca>
2026-02-23 14:47:56 -0500

    Set the color scheme and tint.

--- Clock/Views/LandingView.swift
+++ Clock/Views/LandingView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView(selection: Binding.constant(1)) {
+            
+            WorldClockView()
+                .tabItem {
+                    Image(systemName: "globe")
+                    Text("World Clock")
+                }
+                .tag(1)
+            
+            AlarmsView()
+                .tabItem {
+                    Image(systemName: "alarm.fill")
+                    Text("Alarm")
+                }
+                .tag(2)
+            
+            StopwatchView()
+                .tabItem {
+                    Image(systemName: "stopwatch.fill")
+                    Text("Stopwatch")
+                }
+                .tag(3)
+            }
+        .preferredColorScheme(.dark)
+        .tint(.orange)
     }
 }
 
--- Clock/Views/StopwatchView .swift
+++ Clock/Views/StopwatchView .swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct StopwatchView_: View {
+struct StopwatchView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    StopwatchView_()
+    StopwatchView()
 }
