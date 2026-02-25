58a35c2239e38d8d58a54d3d724b57e3c246c58e
Samuel Thelen <sthelen@lcs.on.ca>
2026-02-25 11:45:07 -0500

    Two times added.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                // Ottawa
+                        HStack {
+                            // Left side
+                            VStack {
+                                Text("Today, +0HRS")
+                                Text("Ottawa")
+                                    .font(.system(.largeTitle, design: .default, weight: .thin))
+                            }
+                            
+                            Spacer()
+                            
+                            // Right side
+                            Text("6:35")
+                                .font(.system(size: 64.0, weight: .thin, design: .default))
+                            Text("AM")
+                                .font(.system(.largeTitle, design: .default, weight: .thin))
+                            
+                        }
+                        // Vancouver
+                        HStack {
+                            // Left side
+                            VStack {
+                                Text("Today, -3HRS")
+                                Text("Vancouver")
+                                    .font(.system(.largeTitle, design: .default, weight: .thin))
+                            }
+                            
+                            Spacer()
+                            
+                            // Right side
+                            Text("3:35")
+                                .font(.system(size: 64.0, weight: .thin, design: .default))
+                            Text("AM")
+                                .font(.system(.largeTitle, design: .default, weight: .thin))
+                        }
+            }
                 .navigationTitle("World Clock")
                 .toolbar {
                     
